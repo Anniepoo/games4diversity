64a30807bc7784e165b071541f5956217d0ff47e
root <logicmoo@gmail.com>
2014-03-23 01:41:56 -0700

    lennard_jones and 100 speed

--- server/vworld.pl
+++ server/vworld.pl
@@ -15,7 +15,6 @@ is_stereotype(gay).
 is_stereotype(wmale25).
 % same as christian but wanted to make sure we handle a third.  Could have used sutypes of LGBT
 
-
 % location type and EffectRange
 is_loc_type(church).
 is_loc_type(sportsbar).
@@ -26,9 +25,9 @@ is_loc_type(disco).
 setup_type(priest,200,church,1,christian).
 setup_type(activist,200,disco,1,gay).
 setup_type(basher,200,sportsbar,1,wmale25).
-setup_type(church,200,church,1,christian).
-setup_type(sportsbar,200,sportsbar,1,wmale25).
-setup_type(disco,200,disco,1,gay).
+setup_type(church,300,church,1,christian).
+setup_type(sportsbar,300,sportsbar,1,wmale25).
+setup_type(disco,300,disco,1,gay).
 
 setup_type(wmale25,100,sportsbar,3,wmale25).
 setup_type(gay,100,disco,5,gay).
@@ -39,8 +38,7 @@ setup_type(christian,100,church,5,christian).
 move_every(4).
 
 % xS,yS - xE,yE, step every 20 seconds.
-world_range(1,1,1000,1000,200).
-
+world_range(1,200,1000,1000,2000).
 
 % -----------------------
 % External API
@@ -138,7 +136,7 @@ clear_world:-
 
 add_persons_places:- setup_type(Priest,_Range200,_FavLoc,Num,_StereoType),
       between(1,Num,N),atom_concat(Priest,N,Whatnot),assert_now(noun_type(Whatnot,Priest)),
-      random_loc(X,Y),assert_now(loc(Whatnot,X,Y)),fail.
+      random_loc(X,Y),once(( loc(Whatnot,_,_) -> true ; assert_now(loc(Whatnot,X,Y) ))),fail.
 add_persons_places.
 
 
@@ -157,9 +155,9 @@ debugFmt(F,A):-format(user_error,F,A),flush_output(user_error).
 start_move_threads:-started_move_threads,!.
 start_move_threads:-
    asserta(started_move_threads),
-   thread_create(move_all_thread,ID1,[alias(move_all_thread),at_exit(retract_self)]),
+%   thread_create(move_all_thread,ID1,[alias(move_all_thread),at_exit(retract_self)]),
    thread_create(interpolate_thread,ID2,[alias(interpolate_thread),at_exit(retract_self)]),
-   asserta(movement_thread(ID1)),
+%   asserta(movement_thread(ID1)),
    asserta(movement_thread(ID2)).
 
 retract_self:-thread_self(ID),retractall(movement_thread(ID)).
@@ -174,12 +172,56 @@ interpolate_thread:-repeat,sleep(1),once(move_all_one_sec),fail.
 move_all_one_sec:-noun_type(P1,Type),not(is_loc_type(Type)),move_for_one_sec(P1),fail.
 move_all_one_sec:-!.
 
-move_for_one_sec(P1):-
+move_for_one_sec(P1):- fail,
    loc(P1,X1,Y1),
    loc_goal(P1,X3,Y3),
    get_polar_coords(X3-X1,Y3-Y1,Angle,_GoDist),
    carts_for_polar_ofset(X1,Y1,Angle,100,X2,Y2),
    set_loc(P1,X2,Y2),!.
+
+move_for_one_sec(P1) :-
+   loc(P1,X1,Y1),
+   setof(P, noun_type_type(P, person), People), 
+   lennard_jones(P1, People, 0,0, FX, FY),
+   speed_cofactor(S),
+   X2 is X1 + FX * S,
+   Y2 is Y1 + FY * S,
+   set_loc(P1,X2,Y2),!,
+   dist(X1,Y1,X2,Y2,D),
+   debugFmt('~w trying move ~w to get from ~w,~w to ~w,~w ~n',[P1, D, X1,Y1,X2,Y2]).
+
+
+% this keeps us out of some very ugly edge cases as we get near R = 0
+stability(8.0).
+speed_cofactor(200.0).
+lj_coefficients(4096.0, -256000.0).
+
+lennard_jones(_P, [], FInX, FInY, FInX, FInY).
+lennard_jones(P, [P|T], FInX, FInY, FX, FY) :-
+    lennard_jones(P, T, FInX, FInY, FX, FY).
+lennard_jones(P, [B|T], FInX, FInY, FX, FY) :-
+    lj_coefficients(LJA, LJB),
+    dist(P, B, R),
+    unit_vector(P, B, R, UX, UY),
+    NFX is FInX + UX * ( LJA / R / R + LJB / R / R / R ),
+    NFY is FInY + UY * ( LJA / R / R + LJB / R / R / R ),
+    lennard_jones(P, T, NFX, NFY, FX, FY).
+
+dist(P, B, R) :-
+    stability(Stability),
+    loc(P, XA, YA),
+    loc(B, XB, YB),
+    DX is XA - XB,
+        DY is YA - YB,
+    R is sqrt(DX * DX + DY * DY) + Stability.
+    
+unit_vector(P, B, R, UX, UY) :-
+    loc(P, XA, YA),
+    loc(B, XB, YB),
+    DX is XA - XB,
+        DY is YA - YB,
+    UX is DX / R,
+    UY is DY / R.
 
 get_polar_coords(DX,DY,Ang,Dist):-Dist is sqrt(DX*DX+DY*DY), Ang is atan2(DY,DX).
 
@@ -205,13 +247,14 @@ change_loc_goal(P1):-
    loc(P1, X1,Y1),!,
    world_range(SX,SY,EX,EY,DistPer20),
    GoDist is random(DistPer20),
-   Angle is random(360)/57.29577951,
+   Angle is (random(360)/57.29577951),
    carts_for_polar_ofset(X1,Y1,Angle,GoDist,X2,Y2),
    make_between(X2,SX,EX,X3),
    make_between(Y2,SY,EY,Y3),
-   set_loc_goal(P1,X3,Y3).
+   set_loc_goal(P1,X3,Y3),
+   debugFmt('~w trying to get from ~w,~w to ~w,~w ~n',[P1, X1,Y1,X3,Y3]).
 
-change_loc_goal(P1):-
+change_loc_goal(P1):- 
    random_loc(X,Y),
    set_loc(P1, X,Y),
    set_loc_goal(P1,X,Y),!.
