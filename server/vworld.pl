59e9da9eafe2168609a0eee5ccb17f925284cbf4
root <logicmoo@gmail.com>
2014-03-22 18:49:47 -0700

    movement code started

--- server/vworld.pl
+++ server/vworld.pl
@@ -10,22 +10,22 @@ is_stereotype(wmale25).
 % same as christian but wanted to make sure we handle a third.  Could have used sutypes of LGBT
 
 
-% location type and range
-is_loc_type(church,200).
-is_loc_type(sportsbar,200).
-is_loc_type(disco,200).
+% location type and EffectRange
+is_loc_type(church).
+is_loc_type(sportsbar).
+is_loc_type(disco).
 
 
-% setup_type(PType,Range,Haunt,Create,Cate).
+% setup_type(PType,EffectRange,Haunt,Create,Cate).
 setup_type(priest,200,church,1,christian).
 setup_type(christian,100,church,5,christian).
 setup_type(activist,200,disco,1,gay).
 setup_type(gay,100,disco,5,gay).
 setup_type(basher,200,sportsbar,1,wmale25).
 setup_type(wmale25,100,sportsbar,3,wmale25).
-setup_type(church,0,church,1,christian).
-setup_type(sportsbar,0,sportsbar,1,wmale25).
-setup_type(disco,0,disco,1,gay).
+setup_type(church,200,church,1,christian).
+setup_type(sportsbar,200,sportsbar,1,wmale25).
+setup_type(disco,200,disco,1,gay).
 
 
 % every 20 seconds.
@@ -42,9 +42,9 @@ world_range(1,1,1000,1000,200).
 
 
 % returns a list 
-get_vworld(List):- P=noun_state(_P1,_X,_Y,_EmoIcon,_BodyIcon), findall(P,P,List).
+get_vworld(List):- P = noun_state(_P1,_X,_Y,_EmoIcon,_BodyIcon), findall(P,P,List).
 
-set_loc_goal(P1,X,Y):- retractall(loc_goal(P1,_,_)),assert_now(loc_goal(P1,X,Y)).
+set_loc_goal(P1,X,Y):- retractall_now(loc_goal(P1,_,_)),assert_now(loc_goal(P1,X,Y)).
    
 reset_world :- clear_world, add_persons_places.
 
@@ -67,8 +67,8 @@ noun_stype(Disco1,Gay):-noun_type(Disco1,Disco),setup_type(Disco,_,_,_,Gay).
 
 
 
-noun_state(P1,X,Y,EmoIcon,BodyIcon):- noun_type(P1,Body),
-   once((loc(P1,X,Y),
+noun_state(P1,X,Y,EmoIcon,BodyIcon):- noun_type(P1,Body),not(is_loc_type(Body)),
+   once((loc(P1,X,Y), 
    atom_concat(Body,'.PNG',BodyIcon),
    reaction_icon(P1,EmoIcon))).
 	
@@ -82,7 +82,9 @@ noun_state(P1,X,Y,EmoIcon,BodyIcon):- noun_type(P1,Body),
 % -----------------------
 
 reaction(P1,P2,Emo,Strengh):-
-     loc(P1,X1,Y1),  loc(P2,X2,Y2), dist(X1,Y1,X2,Y2,Strengh),
+   loc(P1,X1,Y1),
+   loc(P2,X2,Y2), 
+   dist(X1,Y1,X2,Y2,Strengh),
    noun_react(P1,P2,Emo).
 
 
@@ -117,8 +119,10 @@ random_between(S,E,R):-Range is E - S, RM is random(Range),R is RM+S.
 dist(X1,Y1,X2,Y2,D):- DX is X2-X1, DY is Y2-Y1, D is sqrt(DX*DX+DY*DY).
 
 clear_world:-
-              retractall(loc(_,_,_)),retractall(loc_goal(_,_,_)),
-              retractall(noun_type(_,_)).
+              retractall_now(loc(_,_,_)),
+              retractall_now(loc_goal(_,_,_)),
+              retractall_now(noun_type(_,_)).
+
 
 add_persons_places:- setup_type(Priest,_Range200,_FavLoc,Num,_StereoType), 
       between(1,Num,N),atom_concat(Priest,N,Whatnot),assert_now(noun_type(Whatnot,Priest)),
@@ -126,10 +130,10 @@ add_persons_places:- setup_type(Priest,_Range200,_FavLoc,Num,_StereoType),
 add_persons_places.
 
 
-assert_now(X):-debugFmt(X),assert(X).
+assert_now(X):-debugFmt(add(X)),assert(X).
+retractall_now(X):-debugFmt(del(X)),retractall(X).
 
-
-debugFmt(X):-debugFmt('Debug: ~w. ~n',[X]).
+debugFmt(X):-debugFmt('Debug: ~%~w.~n',[X]).
 
 debugFmt(F,A):-format(user_error,F,A),flush_output(user_error).
 
@@ -137,9 +141,37 @@ debugFmt(F,A):-format(user_error,F,A),flush_output(user_error).
 % world play preds
 % -----------------------
 
-move_all.
 
-% move_all:-noun_type(P1,Type),..
+move_all:-debugFmt('startring to move_all!'),fail.
+move_all:-noun_type(P1,Type),not(is_loc_type(Type)),change_loc_goal(P1),fail.
+move_all:-debugFmt('completed to move_all!'),fail.
+move_all:-!.
+
+
+make_between(In,Low,High,Out):-between(Low,High,In),!,Out=In.
+make_between(In,Low,_High,Out):-In < Low,!,Out==Low.
+make_between(In,_Low,High,Out):-In > High,!,Out==High.
+make_between(In,Low,High,Out):-throw(make_between(In,Low,High,Out)).
+
+change_loc_goal(P1):- 
+   loc(P1, X1,Y1),!,
+   world_range(SX,SY,EX,EY,DistPer20),
+   GoDist is random(DistPer20),
+   Angle is random(360)/57.29577951,
+   carts_for_polar_ofset(X1,Y1,Angle,GoDist,X2,Y2),
+   make_between(X2,SX,EX,X3),
+   make_between(Y2,SY,EY,Y3),
+   set_loc_goal(P1,X3,Y3).
+
+change_loc_goal(P1):- 
+   random_loc(X,Y),
+   assert_now(loc(P1, X,Y)),
+   set_loc_goal(P1,X,Y),!.
+
+
+carts_for_polar_ofset(X1,Y1,Angle,GoDist,X2,Y2):-X2 is X1 + cos(Angle)*GoDist,Y2 is Y1 + sin(Angle)*GoDist.
+
+%% move_all:-noun_type(P1,Type),
 
 
 
