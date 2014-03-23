9695149804c858c07f28fd93e3a015a37f4528cd
Anniepoo <annie66us@yahoo.com>
2014-03-23 14:46:29 -0700

    last work before pushing to a new branch on github

--- server/vworld.pl
+++ server/vworld.pl
@@ -1,14 +1,6 @@
 :- module(vworld, [
             get_vworld/1,
-            reset_world/0,
-            clear_world/0,
-            add_persons_places/0,
-            move_all/0,
-            set_loc_goal/3,
-            is_loc_type/1,
-            noun_type/2,
-            start_move_threads/0,
-            stop_move_threads/0]).
+            reset_world/0]).
 
 /** <module> The virtual world is ran here
 
@@ -67,7 +59,9 @@ world_range(1,200,1000,1000,800).
 % returns a list
 
 % Jan fixes!
-get_vworld(List):-  findall(
+get_vworld(List):-
+   make_state_current,
+   findall(
    noun_state(P1,X,Y,NounType,EmoIcon,BodyIcon,ToolTip),
    noun_state(P1,X,Y,NounType,EmoIcon,BodyIcon,ToolTip), List).
 
@@ -77,10 +71,6 @@ set_loc_goal(P1,X1,Y1):-
    retractall(loc_goal(P1,_,_)),assert_now(loc_goal(P1,X2,Y2)).
 
 reset_world :- clear_world, add_persons_places.
-
-% clear_world
-% add_persons_places
-
 
 % -----------------------
 % existential predicates
@@ -103,7 +93,6 @@ noun_state(P1,X,Y,NounTT,EmoIcon,BodyIcon,ToolTip):-
    reaction_icon(P1,EmoIcon),
    noun_info(P1,ToolTip))).
 
-
 noun_info(P1,Info):- ignore(noun_emo_vectors(P1,EmoVectors)), sformat(Info,'~w',[info(P1,EmoVectors)]),!.
 noun_info(P1,P1).
 
@@ -124,8 +113,6 @@ drv(D0,_R,0.0):- D is D0,D < 1,!.
 %%drv(D0,R0,V):-R is R0,D is D0, D < R ,  V is D,!.
 drv(D0,R0,V):-R is R0,D is D0, D < R ,  V is 3 *(R / D),!.
 drv(_,_,0.01).
-
-
 
 noun_react(P1,P2,R):-noun_stype(P1,T1),noun_stype(P2,T2),type_react(T1,T2,R).
 
@@ -174,9 +161,13 @@ clear_world:- retractall_now(loc(_,_,_)),
               retractall_now(noun_type(_,_)).
 
 
-add_persons_places:- setup_type(Priest,_Range200,_FavLoc,Num,_StereoType),
-      between(1,Num,N),atom_concat(Priest,N,Whatnot),assert_now(noun_type(Whatnot,Priest)),
-      random_loc(X,Y),once(( loc(Whatnot,_,_) -> true ; assert_now(loc(Whatnot,X,Y) ))),fail.
+add_persons_places:-
+   setup_type(Priest,_Range200,_FavLoc,Num,_StereoType),
+   between(1,Num,N),
+   atom_concat(Priest,N,Whatnot),
+   assert_now(noun_type(Whatnot,Priest)),
+   random_loc(X,Y),
+   once(( loc(Whatnot,_,_) -> true ; assert_now(loc(Whatnot,X,Y) ))),fail.
 add_persons_places.
 
 
@@ -191,53 +182,68 @@ debugFmt(F,A):-format(user_error,F,A),flush_output(user_error).
 % -----------------------
 % world play preds
 % -----------------------
-:-dynamic(started_move_threads/0).
-:- dynamic movement_thread/1.
+:- dynamic last_move_time/1.
 
-start_move_threads:-started_move_threads,!.
-start_move_threads:-
-   asserta(started_move_threads),
-%   thread_create(move_all_thread,ID1,[alias(move_all_thread),at_exit(retract_self)]),
-   thread_create(interpolate_thread,ID2,[alias(interpolate_thread),at_exit(retract_self)]),
-%   asserta(movement_thread(ID1)),
-   asserta(movement_thread(ID2)).
+%%	last_move_time(-Time:number) is det
+last_move_time(0).
 
-retract_self:-thread_self(ID),retractall(movement_thread(ID)).
+:- initialization mutex_create(_, [alias(state_vars)]).
 
-stop_move_threads:- retract(movement_thread(ID)),thread_signal(ID,thread_exit(kill_move_threads)),fail.
-stop_move_threads.
+%%	make_state_current is det
+%
+%	calling ensures that the world state is current with
+%	the wall time
+%
+%	ok to call frequently
+%
+make_state_current :-
+	make_state_current_.
+%	with_mutex(state_vars, make_state_current_).
 
-move_all_thread:-repeat,sleep(20),once(move_all),fail.
-interpolate_thread:-
-	repeat,
-	sleep(1),
-	catch(once(move_all_one_sec),
+make_state_current_ :-
+	get_time(Time),
+	last_move_time(Last),
+	Last + 1.0 >= Time.
+make_state_current_ :-
+	debug(vworld_ticks, 'into make_state_current_', []),
+	ignore(
+	    catch(once(move_all_one_sec),
 	     Oops,
-	     debug(vworld_ticks, 'Error in move_all_one_sec ~w', [Oops])),
+	     debug(vworld_ticks, 'Error in move_all_one_sec ~w', [Oops]))),
 	debug(vworld_ticks, 'tick', []),
-	fail.
+	get_time(T),
+	retractall(last_move_time(_)),
+	asserta(last_move_time(T)),
+	debug(vworld_ticks, 'set the time to ~w', [T]).
 
 move_all_one_sec:-
 	noun_type(P1,Type),
 	not(is_loc_type(Type)),
 	move_for_one_sec(P1),
 	fail.
+move_all_one_sec :- !.
 
 in_test_annies_work_mode(true).
 
 move_for_one_sec(P1) :-
    in_test_annies_work_mode(true),
    !,
+   debug(vworld_moves, 'moving ~w' , [P1]),
    loc(P1, X1, Y1),
-   X is X1 + 30,
-   Y is Y1 + 50,
+   X is X1 + 3,
+   Y is Y1 + 5,
+   debug(vworld_moves,  'moving ~w to (~w, ~w)', [P1, X, Y]),
    set_loc(P1, X, Y),
+   debug(vworld_moves, 'back from set_loc', []),
    (
        P1 \= priest1
    ;
        debug(vworld_ticks, 'priest1 at (~w,~w)', [X,Y])
-   ).
+   ),!.
+move_for_one_sec(P) :-
+	debug(vworld_ticks, 'Can\'t move ~w', [P]),!.
 
+/*
 move_for_one_sec(P1):-
    loc_goal(P1,X3,Y3), %% only use if there was a goal_loc
    loc(P1,X1,Y1),
@@ -256,6 +262,7 @@ move_for_one_sec(P1) :-
    set_loc(P1,X2,Y2),!,
    dist(X1,Y1,X2,Y2,D),
    nop(debugFmt('~w trying move ~w to get from ~w,~w to ~w,~w ~n',[P1, D, X1,Y1,X2,Y2])).
+*/
 
 noun_emo_most(P1,Emo,Strengh):-noun_emo_vectors(P1,[Strengh-Emo|_]).
 
@@ -276,7 +283,7 @@ rate_situation(Type,P, [B|T], WIn, WOut) :-
 
 
 
-type_sign(P1,P2,-1.0):-noun_stype(P1,S1),noun_stype(P2,S2),S1==S2,!.
+type_sign(P1,P2,-1.0):-noun_stype(P1,S1),noun_stype(P2,S2),S1=S2,!.
 type_sign(_P1,_P2,1.0).
 
 speed_cofactor(200.0).
@@ -331,13 +338,15 @@ unit_vector(P, B, R, UX, UY) :-
 get_polar_coords(DX,DY,Ang,Dist):-Dist is sqrt(DX*DX+DY*DY), Ang is atan2(DY,DX).
 
 set_loc(P1,X1,Y1):-
+   debug(vworld_moves, 'into set_loc ~w ~w,~w', [P1, X1, Y1]),
    to_int(X1,X2),
    to_int(Y1,Y2),
    world_range(SX,SY,EX,EY,_),
    make_between(X2,SX,EX,X3),
    make_between(Y2,SY,EY,Y3),
    retractall(loc(P1,_,_)),
-   assert(loc(P1,X3,Y3)).
+   assert(loc(P1,X3,Y3)),
+   debug(vworld_moves, 'exiting set_loc ~w ~w,~w', [P1, X3, Y3]).
 
 to_int(X1,X2):-X2 is round(X1).
 
@@ -347,8 +356,8 @@ move_all:-debugFmt('completed to move_all!'),fail.
 move_all:-!.
 
 
-make_between(In,Low,_High,Out):-In < Low,!,Out==Low.
-make_between(In,_Low,High,Out):-In > High,!,Out==High.
+make_between(In,Low,_High,Out):-In < Low,!,Out=Low.
+make_between(In,_Low,High,Out):-In > High,!,Out=High.
 make_between(In,_Low,_High,In).
 
 change_loc_goal(P1):-
@@ -370,7 +379,3 @@ change_loc_goal(P1):-
 
 carts_for_polar_ofset(X1,Y1,Angle,GoDist,X2,Y2):-X2 is X1 + cos(Angle)*GoDist,Y2 is Y1 + sin(Angle)*GoDist.
 
-%% move_all:-noun_type(P1,Type),
-
-
-:- start_move_threads.
