3022172c8937fb1aec894dcac9d0afab3c6bb562
logicmoo <logicmoo@gmail.com>
2014-03-23 18:40:44 -0700

    manny modes

--- server/vworld.pl
+++ server/vworld.pl
@@ -1,6 +1,14 @@
 :- module(vworld, [
             get_vworld/1,
-            reset_world/0]).  /* ANNIE - removed stuff */
+            reset_world/0,
+            clear_world/0,
+            add_persons_places/0,
+            move_all/0,
+            set_loc_goal/3,
+            is_loc_type/1,
+            noun_type/2,
+            start_move_threads/0,
+            stop_move_threads/0]).
 
 /** <module> The virtual world is ran here
 
@@ -24,7 +32,7 @@ This file brokers the game state.
 
 is_stereotype(christian).
 is_stereotype(gay).
-is_stereotype(wmale25).
+is_stereotype(average).
 % same as christian but wanted to make sure we handle a third.  Could have used sutypes of LGBT
 
 % location type and EffectRange
@@ -37,12 +45,12 @@ nop(_).
 % setup_type(PType,EffectRange,Haunt,Create,Cate).
 setup_type(priest,200,church,1,christian).
 setup_type(activist,200,disco,1,gay).
-setup_type(basher,200,sportsbar,1,wmale25).
+setup_type(basher,200,sportsbar,1,average).
 setup_type(church,300,church,1,christian).
-setup_type(sportsbar,300,sportsbar,1,wmale25).
+setup_type(sportsbar,300,sportsbar,1,average).
 setup_type(disco,300,disco,1,gay).
 
-setup_type(wmale25,100,sportsbar,3,wmale25).
+setup_type(average,100,sportsbar,3,average).
 setup_type(gay,100,disco,5,gay).
 setup_type(christian,100,church,5,christian).
 
@@ -58,11 +66,7 @@ world_range(1,200,1000,1000,800).
 
 % returns a list
 
-get_vworld(List):-
-   make_state_current,   /* ANNIE */
-   findall(                      % Jan fixes!
-   noun_state(P1,X,Y,NounType,EmoIcon,BodyIcon,ToolTip),
-   noun_state(P1,X,Y,NounType,EmoIcon,BodyIcon,ToolTip), List).
+get_vworld(ListO):-  P=noun_state(_P1,_X,_Y,_NounType,_EmoIcon,_BodyIcon,_ToolTip), findall(P,P,List),!,ListO=List.
 
 set_loc_goal(P1,X1,Y1):-
    to_int(X1,X2),
@@ -70,6 +74,10 @@ set_loc_goal(P1,X1,Y1):-
    retractall(loc_goal(P1,_,_)),assert_now(loc_goal(P1,X2,Y2)).
 
 reset_world :- clear_world, add_persons_places.
+
+% clear_world
+% add_persons_places
+
 
 % -----------------------
 % existential predicates
@@ -85,19 +93,62 @@ noun_stype(Disco1,Gay):-noun_type(Disco1,Disco),setup_type(Disco,_,_,_,Gay).
 noun_type_type(P1,T):-noun_type(P1,T1),(is_loc_type(T1) -> T=place; T=person),!.
 
 noun_state(P1,X,Y,NounTT,EmoIcon,BodyIcon,ToolTip):-
-   noun_type(P1,Body),
+   noun_type(P1,_),
    once((loc(P1,X,Y),
          noun_type_type(P1,NounTT),
-   atom_concat(Body,'.PNG',BodyIcon),
+   once((body_icon(P1,BodyIcon))),
    reaction_icon(P1,EmoIcon),
    noun_info(P1,ToolTip))).
 
-noun_info(P1,Info):- ignore(noun_emo_vectors(P1,EmoVectors)), sformat(Info,'~w',[info(P1,EmoVectors)]),!.
+body_icon(P1,BodyIcon):-gender(P1,G),noun_type(P1,T),body_icon(G,T,BodyIcon),!.
+
+
+:-dynamic(known_gender/2).
+known_gender(priest1,male).
+gender(P1,G):- known_gender(P1,G),!.
+gender(P1,G):- (0 is random(2)->G=male;G=female),assert(known_gender(P1,G)),!.
+
+
+
+
+
+noun_info(P1,Info):-body_icon(P1,BI),reaction_icon(P1,RI),gender(P1,G), ignore(noun_emo_vectors(P1,EmoVectors)), sformat(Info,'~w',[info(G,P1,BI,RI,EmoVectors)]),!.
 noun_info(P1,P1).
 
 % place a Person is traveling to
 
 :-dynamic(loc_goal/3).
+
+% -----------------------
+% Graphics
+% -----------------------
+old_icons :- fail.
+
+
+%% heads...
+reaction_icon_typed(_Person,_Gender,_Type,Emo,Icon):- old_icons, atom_concat(Emo,'1.PNG',Icon).
+
+reaction_icon_typed(_P1,_G,priest,angry,'WPreac_Angryl.png'). % all priest are male or females posing as males
+reaction_icon_typed(_P1,_G,priest,happy,'WPreac_Happy.png').
+reaction_icon_typed(_P1,male,_,happy,'WhMale_Happy.png').
+reaction_icon_typed(_P1,female,_,happy,'WWoman_Happy.png').
+reaction_icon_typed(_P1,male,_,neutral,'WhMale_Neutral.png').
+reaction_icon_typed(_P1,female,_,neutral,'WWoman_Neutral.png').
+reaction_icon_typed(_P1,male,_,angry,'WhMale_Angry.png').
+reaction_icon_typed(_P1,female,_,angry,'BWoman_Angry.png').
+reaction_icon_typed(_P1,male,_,fear,'WhMale_Fear.png').
+reaction_icon_typed(_P1,female,_,fear,'WWoman_Fear.png').
+
+
+reaction_icon_typed(_P1,male,gay,happy,'BGayMa_Happy.png').
+reaction_icon_typed(_P1,female,gay,happy,'WWoman_Happy.png').
+reaction_icon_typed(_P1,male,gay,neutral,'WhMale_Neutral.png').
+reaction_icon_typed(_P1,female,gay,neutral,'WWoman_Neutral.png').
+reaction_icon_typed(_P1,male,gay,angry,'WhMale_Angry.png').
+reaction_icon_typed(_P1,female,gay,angry,'WGayFe_Angry.png').
+reaction_icon_typed(_P1,male,gay,fear,'WhMale_Fear.png').
+reaction_icon_typed(_P1,female,gay,fear,'WWoman_Fear.png').
+
 
 % -----------------------
 % world util predicates
@@ -113,10 +164,32 @@ drv(D0,_R,0.0):- D is D0,D < 1,!.
 drv(D0,R0,V):-R is R0,D is D0, D < R ,  V is 3 *(R / D),!.
 drv(_,_,0.01).
 
+
+
 noun_react(P1,P2,R):-noun_stype(P1,T1),noun_stype(P2,T2),type_react(T1,T2,R).
 
+
+%% bodies...
+body_icon(_Gender,Type,Icon):- old_icons,atom_concat(Type,'.PNG',Icon).
+
+body_icon(_,average,'wmale25.PNG').
+body_icon(_,average,'WhMale.PNG').
+body_icon(_,priest,'WPreac.png').
+body_icon(male,gay,'BGayMa.png').
+body_icon(_,gay,'WGayFe.png').
+body_icon(male,priest,'WPreac.png').
+body_icon(_,X,G):-atom_concat(X,'.PNG',G).
+body_icon(male,_,'wmale25.PNG').
+
+
+
 reaction_icon(P1,EmoIconPNG):-
-   noun_emo_most(P1,Emo,Strengh),
+  notrace(( noun_emo_most(P1,Emo,_Strengh))),
+   gender(P1,G),noun_type(P1,T),
+   reaction_icon_typed(P1,G,T,Emo,EmoIconPNG),!.
+
+reaction_icon(P1,EmoIconPNG):-
+   notrace((noun_emo_most(P1,Emo,Strengh))),
    strengh_scale(Strengh,SS),
    atom_concat(Emo,SS,EmoIcon),
    atom_concat(EmoIcon,'.PNG',EmoIconPNG).
@@ -127,13 +200,13 @@ is_emo(fear).
 is_emo(happy).
 
 type_react(gay,gay,happy).
-type_react(wmale25,gay,fear).
+type_react(average,gay,fear).
 type_react(christian,gay,anger).
-type_react(gay,wmale25,happy).
-type_react(wmale25,wmale25,neutral).
-type_react(christian,wmale25,neutral).
+type_react(gay,average,happy).
+type_react(average,average,neutral).
+type_react(christian,average,neutral).
 type_react(gay,christian,fear).
-type_react(wmale25,christian,happy).
+type_react(average,christian,happy).
 type_react(christian,christian,happy).
 
 strengh_scale(_,1).
@@ -160,13 +233,10 @@ clear_world:- retractall_now(loc(_,_,_)),
               retractall_now(noun_type(_,_)).
 
 
-add_persons_places:-
-   setup_type(Priest,_Range200,_FavLoc,Num,_StereoType),
-   between(1,Num,N),
-   atom_concat(Priest,N,Whatnot),
-   assert_now(noun_type(Whatnot,Priest)),
-   random_loc(X,Y),
-   once(( loc(Whatnot,_,_) -> true ; assert_now(loc(Whatnot,X,Y) ))),fail.
+add_persons_places:-assert_now(loc(church1, 294, 274)),assert_now(loc(disco1, 629 , 241)),assert_now(loc(sportsbar1, 903,341)),fail.
+add_persons_places:- setup_type(Priest,_Range200,_FavLoc,Num,_StereoType),
+      between(1,Num,N),atom_concat(Priest,N,Whatnot),assert_now(noun_type(Whatnot,Priest)),
+      random_loc(X,Y),once(( loc(Whatnot,_,_) -> true ; assert_now(loc(Whatnot,X,Y) ))),fail.
 add_persons_places.
 
 
@@ -181,95 +251,41 @@ debugFmt(F,A):-format(user_error,F,A),flush_output(user_error).
 % -----------------------
 % world play preds
 % -----------------------
-%
+:-dynamic(started_move_threads/0).
+start_move_threads:-started_move_threads,!.
+start_move_threads:-
+   asserta(started_move_threads),
+%   thread_create(change_movement_goals,ID1,[alias(change_movement_goals),at_exit(retract_self)]),
+   thread_create(interpolate_thread,ID2,[alias(interpolate_thread),at_exit(retract_self)]),
+%   asserta(movement_thread(ID1)),
+   asserta(movement_thread(ID2)).
 
-		 /*******************************
-		 *  ANNIE	  Non-threaded game state
-		 *   update. The
-		 *******************************/
+retract_self:-thread_self(ID),retractall(movement_thread(ID)).
 
-:- dynamic last_move_time/1.
+stop_move_threads:- retract(movement_thread(ID)),thread_signal(ID,thread_exit(kill_move_threads)),fail.
+stop_move_threads.
 
-%%	last_move_time(-Time:number) is det
-last_move_time(0).
+%% blocks and emulates players every 20 seconds
+%change_movement_goals:-repeat,sleep(20),once(move_all),fail.
 
-:- initialization mutex_create(_, [alias(state_vars)]).
-
-%%	make_state_current is det
-%
-%	calling ensures that the world state is current with
-%	the wall time
-%
-%	ok to call frequently
-%
-make_state_current :-
-	with_mutex(state_vars, make_state_current_).
-
-make_state_current_ :-
-	get_time(Time),
-	last_move_time(Last),
-	Last + 1.0 >= Time.
-make_state_current_ :-
-	debug(vworld_ticks, 'into make_state_current_', []),
-	ignore(
-	    catch(once(move_all_one_sec),
-	     Oops,
-	     debug(vworld_ticks, 'Error in move_all_one_sec ~w', [Oops]))),
-	debug(vworld_ticks, 'tick', []),
-	get_time(T),
-	retractall(last_move_time(_)),
-	asserta(last_move_time(T)),
-	debug(vworld_ticks, 'set the time to ~w', [T]).
-
-move_all_one_sec:-
-	noun_type(P1,Type),
-	not(is_loc_type(Type)),
-	move_for_one_sec(P1),
-	fail.
-move_all_one_sec :- !.
-
-		 /*******************************
-		 *    ANNIE - wanted to be able to debug
-		 *  game engine outer loop without debugging motion
-		 *  so I made simple linear motion.  define false
-		 and uncomment big chunk below to
-		 go back to nromal
-		 *******************************/
+%% blocks and advances frames 1 per second
+interpolate_thread:-repeat,sleep(1),once(move_all_one_sec),fail.
 
 
-in_test_annies_work_mode(false).
+% advances one frame
+move_all_one_sec:-noun_type(P1,Type),not(is_loc_type(Type)),move_for_one_sec(P1),fail.
+move_all_one_sec:-!, make.  % to check for changed disk files!
 
-move_for_one_sec(P1) :-
-   in_test_annies_work_mode(true),
-   !,
-   debug(vworld_moves, 'moving ~w' , [P1]),
-   loc(P1, X1, Y1),
-   X is X1 + 3,
-   Y is Y1 + 5,
-   debug(vworld_moves,  'moving ~w to (~w, ~w)', [P1, X, Y]),
-   set_loc(P1, X, Y),
-   debug(vworld_moves, 'back from set_loc', []),
-   (
-       P1 \= priest1
-   ;
-       debug(vworld_ticks, 'priest1 at (~w,~w)', [X,Y])
-   ),!.
-move_for_one_sec(P) :-
-   in_test_annies_work_mode(true),
-	debug(vworld_ticks, 'Can\'t move ~w', [P]),!.
-
-
-move_for_one_sec(P1):-
+move_for_one_sec(P1):- 
    loc_goal(P1,X3,Y3), %% only use if there was a goal_loc
    loc(P1,X1,Y1),
    get_polar_coords(X3-X1,Y3-Y1,Angle,_GoDist),
    carts_for_polar_ofset(X1,Y1,Angle,100,X2,Y2),
-
    set_loc(P1,X2,Y2),!.
 
 move_for_one_sec(P1) :-
    loc(P1,X1,Y1),
-   setof(P, noun_type(P, _), People),
+   setof(P, noun_type(P, _), People), 
    lennard_jones_m(P1, People, 0,0, FX, FY),
    speed_cofactor(S),
    X2 is X1 + FX * S,
@@ -277,7 +293,6 @@ move_for_one_sec(P1) :-
    set_loc(P1,X2,Y2),!,
    dist(X1,Y1,X2,Y2,D),
    nop(debugFmt('~w trying move ~w to get from ~w,~w to ~w,~w ~n',[P1, D, X1,Y1,X2,Y2])).
-
 
 noun_emo_most(P1,Emo,Strengh):-noun_emo_vectors(P1,[Strengh-Emo|_]).
 
@@ -312,7 +327,7 @@ lj_coefficients(move,_,_,4096.0, -256000.0).
 lj_coefficients(tension,_,_,4096.0, -256000.0).
 
 
-
+  
 % movement version
 lennard_jones_m(P, T, NFX, NFY, FX, FY):-lennard_jones(move, P, T, NFX, NFY, FX, FY).
 
@@ -320,7 +335,7 @@ lennard_jones_m(P, T, NFX, NFY, FX, FY):-lennard_jones(move, P, T, NFX, NFY, FX,
 lennard_jones_a_w(P, T, NFX, NFY, FX, FY):-
    lennard_jones(tension, P, T, NFX, NFY, FX, FY),!.
 
-
+   
 lennard_jones(_Type,_P, [], FInX, FInY, FInX, FInY).
 lennard_jones(Type,P, [P|T], FInX, FInY, FX, FY) :- % skip oneself
     lennard_jones(Type,P, T, FInX, FInY, FX, FY).
@@ -337,11 +352,11 @@ lj_dist(Type, P, B,  R) :-
    stability(Type, P, B, Stability),
    mag(Type, P, B, Mag),
    dist(P,B,D),
-
+   
   %% nop((noun_type(B,BType), type_effect_range(BType,RE), drv(D,RE*2,V1),V is V1*5 )),
    V is D,
    R is (V* Mag) + Stability.
-
+    
 unit_vector(P, B, R, UX, UY) :-
     loc(P, XA, YA),
     loc(B, XB, YB),
@@ -386,11 +401,16 @@ change_loc_goal(P1):-
    set_loc_goal(P1,X3,Y3),
    debugFmt('~w trying to get from ~w,~w to ~w,~w ~n',[P1, X1,Y1,X3,Y3]).
 
-change_loc_goal(P1):-
+change_loc_goal(P1):- 
    random_loc(X,Y),
    set_loc(P1, X,Y),
    set_loc_goal(P1,X,Y),!.
 
 
 carts_for_polar_ofset(X1,Y1,Angle,GoDist,X2,Y2):-X2 is X1 + cos(Angle)*GoDist,Y2 is Y1 + sin(Angle)*GoDist.
+
+%% move_all:-noun_type(P1,Type),
+
+
+:- start_move_threads.
 
